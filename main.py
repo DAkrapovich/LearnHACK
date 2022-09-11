007085df79999f03fef1c547d0d18a8c9fd514ba
DAkrapovich <113048332+DAkrapovich@users.noreply.github.com>
2022-09-11 03:38:23 +0300

    Add files via upload
    RP GAME

---
+++ main.py
@@ -0,0 +1,42 @@
+Search = 0
+Go = 0
+Sell = 0
+
+def One (Search, Go, Sell):
+    input("choose your move: (Go/Search)")
+    if(Search):
+        print("Searching...")
+        print("Your have been searching a gold")
+        input("choose your move: (Search, Sell)")
+    else(Search)
+        print("Searching...")
+        print("Your have been searching a gold")
+        input("choose your move: (Search, Sell)")
+    elif(Sell)
+    print("You selled your gold!")
+
+def Two (Search, Go, Sell)
+    input("choose your move: (Go/Search)")
+    if(Go)
+        print("Going...")
+        print("Your have been searching a new place")
+        input("choose your move: (Search, Sell)")
+    else(Search)
+        print("Searching...")
+        print("Your have been searching a gold")
+        input("choose your move: (Search, Sell)")
+    elif(Sell)
+    print("You selled your gold!")
+
+
+
+
+
+
+
+
+
+
+
+
+
